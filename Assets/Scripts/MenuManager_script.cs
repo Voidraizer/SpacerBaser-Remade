b0650d4f50cc5c30b6bdae29428c200fe715bbe6
Voidraizer <splutikoff@gmail.com>
2016-01-24 16:17:46 -0500

    Created Start screen and menu
    Added basic start screen and basic menu
    Added profile picture slot and profile name + greeting

---
+++ Assets/Scripts/MenuManager_script.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager_script : MonoBehaviour {
+
+    [SerializeField]
+    private Text profileNameObj;
+
+    private bool gotName = false;
+
+	void Start () 
+	{
+        DontDestroyOnLoad( gameObject );
+	}
+	
+	void Update ()
+	{
+        if( !gotName )
+        {
+            if( SceneManager.GetActiveScene().name == "menu" )
+            {
+                profileNameObj = GameObject.Find( "ProfileNameText" ).GetComponent<Text>();
+                profileNameObj.text = "Void!";
+
+                if( profileNameObj != null )
+                {
+                    gotName = true;
+                }
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/SceneManager_script.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneManager_script : MonoBehaviour {
+
+	void Start () 
+	{
+        DontDestroyOnLoad( gameObject );
+    }
+
+    public void LoadScene( string name )
+    {
+        SceneManager.LoadScene( name );
+    }
+}
